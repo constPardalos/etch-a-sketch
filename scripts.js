7137949b8600b83b9aba48d735f5a7f9946fe479
theColorDude <theColorDude@outlook.com>
2022-04-22 09:52:55 +0300

    Create new modes and elements * Add buttons for selecting mode * Add tools and settings sections * Create method for sketching in black * Create method for sketching in custom color * Create method for sketching in random colors * Create method for clearing the grid

--- scripts.js
+++ scripts.js
@@ -1,22 +1,63 @@
-const gridSize = 16;
-const grid = document.querySelector('.grid');
-grid.style.cssText = `grid-template-columns: repeat(${gridSize}, 1fr);
-    grid-template-rows: repeat(${gridSize}, 1fr);`;
-const tiles = [];
+// Sketch Modes
+const activeMode = modeClassic;
 
-// Generate the grid tiles
-function generateGrid (size) {
+function modeClassic() {
+    this.style.cssText = 'background-color: black;';
+}
 
-    for (let i=0; i < size * size; i++) {
-        tiles.push(document.createElement('div'));
-    }
-
-    tiles.forEach((tile) => {
-        tile.classList.add('tile');
-        grid.appendChild(tile);
-    });
+function modeGhost() {
 
 }
 
+function modeColor() {
+    const customColor = document.querySelector('#custom-color');
+    this.style.cssText = `background-color: ${customColor.value};`;
+}
+
+function modeRainbow() {
+    const currentColor = this.style.backgroundColor;
+    const h = Math.floor(Math.random() * (360 + 1));
+    const s = Math.floor(Math.random() * (50 + 1)) + 50;
+    let l = 50;
+
+    if (currentColor === 'white') {
+        this.style.cssText = `background-color: hsl(${h},${s}%,${l}%);`;
+    }
+
+}
+
+function modePicture() {
+    this.style.cssText = `opacity: 0;`;
+}
+
+// Generate Grid
+const grid = document.querySelector('.grid');
+let gridSize = 16;
+const gridTiles = [];
+
 generateGrid (gridSize);
 
+function generateGrid (size) {
+    grid.style.cssText = `grid-template-columns: repeat(${size}, 1fr);
+    grid-template-rows: repeat(${size}, 1fr);`;
+
+    for (let i=0; i < size * size; i++) {
+        gridTiles.push(document.createElement('div'));
+    }
+    gridTiles.forEach((tile) => {
+        tile.classList.add('tile');
+        grid.appendChild(tile);
+        tile.addEventListener('mouseover', modeRainbow);
+        tile.style.cssText = 'background-color: white; opacity: 1;';
+    });
+}
+
+// Clear Grid
+const clearButton = document.querySelector('.clear');
+clearButton.addEventListener('click', clearGrid)
+
+function clearGrid() {
+    gridTiles.forEach((tile) => {
+        tile.style.cssText = 'background-color: white; opacity: 1;';
+    });
+}
